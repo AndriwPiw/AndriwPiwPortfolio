83e6d3b528fee3b04c2c94b30ac93631b269bac5
Saturni-Idae <andreapallasm@gmail.com>
2026-01-12 18:57:18 +0100

    Project update
    Updated gallery view and business card project

--- js/cardSelectorDouble.js
+++ js/cardSelectorDouble.js
@@ -1,10 +1,9 @@
 `use strict`
 
 
-
-
 const cardViewer = document.querySelector(`.cardViewer`)
 const cardViewerImgFront = cardViewer.querySelector('.cardViewer-img--front')
+const cardViewerImgBack = cardViewer.querySelector('.cardViewer-img--back')
 let projectImgs = document.querySelectorAll(`.Project-img`)
 
 projectImgs.forEach((img) => {
@@ -12,6 +11,9 @@ projectImgs.forEach((img) => {
     img.addEventListener(`click`, () => {
         let frontSrc = img.getAttribute(`data-front`)
         cardViewerImgFront.src = frontSrc
+
+        let backSrc = img.getAttribute(`data-back`)
+        cardViewerImgBack.src = backSrc
 
 
     })
